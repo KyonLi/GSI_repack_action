1a5c8941b6bd337a8558fae71eec0a12dd37e3fc
Kyon Li <i@kyonli.com>
2025-08-30 20:21:16 +0800

    fix

--- resetprop.sh
+++ resetprop.sh
@@ -4,7 +4,7 @@ IMG=$1
 MNT=/mnt/system
 mkdir $MNT
 resize2fs "$IMG" 10G
-e2fsck -E unshare_blocks "$IMG"
+e2fsck -p -E unshare_blocks "$IMG"
 mount -t ext4 -o loop,rw "$IMG" $MNT
 cp resetprop_phh $MNT/system/bin/
 chown 0:2000 $MNT/system/bin/resetprop_phh
